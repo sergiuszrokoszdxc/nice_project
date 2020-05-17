b7dc2343ef5c7dc39d8f3a250c2b500cbf6751a9
Sergiusz Rokosz <sergiusz.rokosz@dxc.com>
2020-05-17 10:49:06 +0200

    dockerfile added

---
+++ nice_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='nice_project',
+   version='0.1.1',
+   description='A module for being nice',
+   author='Sergiusz Rokosz',
+   author_email='sergiusz.rokosz@dxc    .com',
+   packages=['nice_project', "nice_project.utils"],
+)
