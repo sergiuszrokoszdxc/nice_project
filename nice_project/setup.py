6f3a702048fd7e58274d37399c0542e109eb4340
Sergiusz Rokosz <sergiusz.rokosz@dxc.com>
2020-05-18 09:13:44 +0200

    plan determined

--- nice_project/setup.py
+++ nice_project/setup.py
@@ -5,6 +5,8 @@ setup(
    version='0.1.1',
    description='A module for being nice',
    author='Sergiusz Rokosz',
-   author_email='sergiusz.rokosz@dxc    .com',
+   author_email='sergiusz.rokosz@dxc.com',
    packages=['nice_project', "nice_project.utils"],
+   include_package_data=True,
+   install_requires=["Flask==1.1.2"]
 )
