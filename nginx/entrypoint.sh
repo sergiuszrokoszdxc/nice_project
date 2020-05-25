2d4dcbce3d602dbc674e2f227d1e0b803d15f4eb
Sergiusz Rokosz <sergiusz.rokosz@dxc.com>
2020-05-25 06:27:54 +0200

    added nginx server image

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,6 +2,6 @@
 
 sed -i -e "s/\${NICE_PROJECT_HOST}/${NICE_PROJECT_HOST}/" \
   -e "s/\${NICE_PROJECT_PORT}/${NICE_PROJECT_PORT}/" \
-  -e "s/\${NGINX_LISTEN_PORT}/${NGINX_LISTEN_PORT}/" /etc/nginx/nginx.conf
+  -e "s/\${NGINX_LISTEN_PORT}/${NGINX_LISTEN_PORT}/" /etc/nginx/nginx.conf;
 
-exec $@
+exec "$@"
